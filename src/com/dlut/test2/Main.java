b638d2d2e0137e9bcd15237c5f41a50b494da55c
dormi.fu <dormi.fu@vipshop.com>
2017-01-16 11:51:41 +0800

    modify something in VIP.

--- src/com/dlut/test2/Main.java
+++ src/com/dlut/test2/Main.java
@@ -1,15 +1,31 @@
 package com.dlut.test2;
 
-public class Main {
-    public static void swap(int a,int b) {
+//public class Main {
+//    public static void swap(int a,int b) {
+//        int temp = a;
+//        a = b;
+//        b = temp;
+//    }
+//    public static void main(String[] args) {
+//        int a=1,b=2;
+//        System.out.println("a="+a+",b="+b);
+//        Main.swap(a, b);
+//        System.out.println("a="+a+",b="+b);
+//    }
+//}
+
+public class Main{
+    public static void swap(int a, int b){
         int temp = a;
         a = b;
         b = temp;
     }
-    public static void main(String[] args) {
-        int a=1,b=2;
-        System.out.println("a="+a+",b="+b);
+
+    public static void main(String[] args){
+        int a = 2, b = 5;
+        System.out.println("a = " + a + ", b = " + b);
         Main.swap(a, b);
-        System.out.println("a="+a+",b="+b);
+
+        System.out.println("a = " + a + ", b = " + b);
     }
 }
--- src/com/dlut/test3/ObjTest1.java
+++ src/com/dlut/test3/ObjTest1.java
@@ -6,7 +6,23 @@ public class ObjTest1 {
             arr[i]=10+i;
         }
     }
+
+    public static void changeArr(String arr[]){
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = arr[i] + "d";
+        }
+    }
     private static void printArr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]);
+            if(i!=arr.length-1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printArr(String arr[]) {
         for(int i=0;i<arr.length;i++) {
             System.out.print(arr[i]);
             if(i!=arr.length-1) {
@@ -17,10 +33,14 @@ public class ObjTest1 {
     }
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5};
+        String[] AA = new String[] {"da2", "dd3"};
         System.out.println("改变数组前");
         ObjTest1.printArr(arr);
+        ObjTest1.printArr(AA);
+        ObjTest1.changeArr(AA);
         ObjTest1.changeArr(arr);
         System.out.println("改变数组后");
         ObjTest1.printArr(arr);
+        ObjTest1.printArr(AA);
     }
 }
